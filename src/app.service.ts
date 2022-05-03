8e9c9179711f6fa3896eab399f73410f6e854d0a
WHO-A-U <djfxl46@naver.com>
2022-05-04 00:13:23 +0900

    trigger test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! who-a-u!!!!3~~~!';
+    return 'CI/CD test ';
   }
 
   getPing(): string {
